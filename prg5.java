3eacf333b65df448a723e38f5f2b41e5f7c10610
unknown <jalbertm@gmail.com>
2022-10-09 09:11:34 +0100

    sexto proyecto prog1 prg5

--- prg5.java
+++ prg5.java
@@ -4,8 +4,7 @@ public class prg5 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Tercer proyectoooooooooooooooooo");
-System.out.println("Tercer proyectoooooooooooooooooo segunda modificacion");
+		System.out.println("Tercer proyectoooooooooooooooooo segunda modificacion");
 	}
 
 }
--- prog1.java
+++ prog1.java
@@ -3,8 +3,7 @@ package entornosversiones;
 public class prog1 {
 
 	public static void main(String[] args) {
-System.out.println("Programa gestión de versiones, primera modificaciónnnnnnnnnnnnnnnn");
-System.out.println("Programa gestión de versiones, segunda modificaciónnnnnnnnnnnnnnnn");
+		System.out.println("Tercer proyectoooooooooooooooooo segunda modificacion");
 	}
 
 }
