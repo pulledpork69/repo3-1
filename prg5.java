df6453706241a7da09e2920ed285a50a95c5f59a
unknown <jalbertm@gmail.com>
2022-10-09 08:38:11 +0100

    Tercer proyecto prog1 prg5

---
+++ prg5.java
@@ -0,0 +1,10 @@
+package entornosversiones;
+
+public class prg5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Tercer proyecto");
+	}
+
+}
