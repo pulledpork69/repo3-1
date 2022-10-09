7aaf49e33d5a32b145d4ca2f4f3a9e1aca65f6c9
unknown <jalbertm@gmail.com>
2022-10-09 07:56:32 +0100

    comienza proyecto

--- prog1.java
+++ prog1.java
@@ -3,7 +3,7 @@ package entornosversiones;
 public class prog1 {
 
 	public static void main(String[] args) {
-System.out.println("Programa gestión de versiones");
+System.out.println("Programa gestión de versiones, primera modificación");
 	}
 
 }
