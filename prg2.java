a13a87505fab19d6c339ad7223e73500cb470a69
unknown <jalbertm@gmail.com>
2022-10-09 08:21:56 +0100

    segundo proyecto

--- prg2.java
+++ prg2.java
@@ -3,7 +3,8 @@ package entornosversiones;
 public class prg2 {
 
 	public static void main(String[] args) {
-		System.out.println("Segundo programa gestión de versiones");
+		System.out.println("Segundo programa gestión de versionesssssssssssssssssss");
+		System.out.println("Segundo proyectooo");
 	}
 
 }
--- prog1.java
+++ prog1.java
@@ -4,7 +4,8 @@ public class prog1 {
 
 	public static void main(String[] args) {
 System.out.println("Programa gestión de versiones, primera modificación");
-System.out.println("Programa gestión de versiones, segunda modificación");
+System.out.println("Programa gestión de versiones, segunda modificaciónnnnnnnnnn");
+System.out.println("Segundo proyectoooo");
 	}
 
 }
