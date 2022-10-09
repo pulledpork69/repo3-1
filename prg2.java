7e34fc4be7d346b93a7a1de16f54179f8167e0c3
unknown <jalbertm@gmail.com>
2022-10-09 08:25:21 +0100

    segundo proyecto v4

--- prg2.java
+++ prg2.java
@@ -5,6 +5,7 @@ public class prg2 {
 	public static void main(String[] args) {
 		System.out.println("Segundo programa gestión de versionesssssssssssssssssss");
 		System.out.println("Segundo proyectooo");
+		System.out.println("Segundo proyectooo version 4");
 	}
 
 }
